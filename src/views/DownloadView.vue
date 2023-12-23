cf1fde0e2db12d45a1db10a093ec3cc003d46a14
HypherionMC <hypherionmc@gmail.com>
2023-12-24 01:31:11 +0200

    Fix installer download link

--- src/views/DownloadView.vue
+++ src/views/DownloadView.vue
@@ -6,7 +6,7 @@
 
         <div class='w-full text-center mt-5'>
           <a :href='dlLink' class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-            Download Flint Installer
+            Download Flint Installer ({{ version }})
             <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
             </svg>
@@ -36,13 +36,15 @@
 import { onMounted, ref } from 'vue'
 
 const dlLink = ref("#");
+const version = ref("Loading");
 
 onMounted(async () => {
-  fetch("https://meta.flintloader.net/v1/versions/loader").then(async res => {
-    var data = await res.json() as any;
+  fetch("https://meta.flintloader.net/v1/versions/installer").then(async res => {
+    const data = await res.json() as any
 
     if (data) {
       dlLink.value = data[0].url;
+      version.value = data[0].version;
     }
   })
 })
