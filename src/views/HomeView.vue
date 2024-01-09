3cfcb48cae5aaafbc71b73d7adaa3e39f0fd82fa
HypherionMC <hypherionmc@gmail.com>
2024-01-09 19:59:01 +0200

    Update Homepage to make the project purpose a bit clearer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -20,8 +20,11 @@ onMounted(() => {
           <h1 class='font-bold text-2xl mb-2'>What is this?</h1>
 
           <p>
-            Flint is a tiny, open-source modloader system, aimed at allowing developers to make client only mods, that players can use while playing on vanilla, paper/bukkit/spigot servers, without worrying about mod compatibility. <br /><br />
-            We will also try to create our API in such a way that modules don't need to update with every single minecraft release. For now this project is a hobby side project, but if there is enough interest, it will be actively maintained and developed.
+            Flint is a tiny, open-source modloader system, aimed at players playing on Vanilla and Plugin Servers, like Spigot/Paper etc. This is <b>NOT</b> a replacement for Fabric, Quilt, NeoForge or Forge, and will not allow you to run mods made for any of them. Flint has it's own API for mods. <br /><br />
+            Flint is a hobby/learning project, and is not intended to replace any other modloader out there, and will not allow you to run a server. If you feel this project is stupid and should die, don't use it. Better modloaders with better mods exist. <br /><br />
+            If there is ever enough interest in this project, it will be continuously be developed and improved on, and may possibly get a compatibility layer for SOME fabric client side mods. <br /><br />
+
+            Any developers potentially interested in developing for this loader, you are free to do so, and ask for help in our Discord. You can also check out the <a href='https://github.com/Flintloader-Modules'>UNOFFICIAL DEMO MODULES</a> repository for some insight into how flint development works.
           </p>
         </div>
 
